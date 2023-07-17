c0c5b98545b1a8db47aeca49310f1b4a7040234a
alein011 <108501275+alein011@users.noreply.github.com>
2023-07-18 06:27:12 +0800

    fixed carousel

--- solesforkiddos/views.py
+++ solesforkiddos/views.py
@@ -7,19 +7,18 @@ def home(request):
     
     seven_days_ago = datetime.now() - timedelta(days=7)
     products = Product.objects.filter(Q(is_sale=True) | Q(modified_date__gte=seven_days_ago)).order_by('-modified_date')[:9]
-    
+    banners = []
     # Get the reviews
     reviews = None
     for product in products:
-        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
-
-    banners = Banner.objects.filter(is_active=True)
-    
-    
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)  
+        
+    banners = Banner.objects.all().order_by('-title')
     
     context = {
         'products': products,
         'reviews': reviews,
-        'banners': banners
+        'banners': banners,
     }
     return render(request, 'home.html', context)
+
