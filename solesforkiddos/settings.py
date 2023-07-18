5741caadd6a91edaea36d4ac59fffd47e938d12d
alein011 <108501275+alein011@users.noreply.github.com>
2023-07-19 07:28:27 +0800

    new

--- solesforkiddos/settings.py
+++ solesforkiddos/settings.py
@@ -111,29 +111,29 @@ AUTH_USER_MODEL = 'accounts.Account'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
+# DATABASES = {
+#      'default': {
+#           'ENGINE': 'django.db.backends.postgresql',
+#           'NAME': 'django',
+#           'USER': 'admin01',
+#           'PASSWORD': config('DB_PASSWORD'),
+#           'HOST': 'solesforkiddosdb.postgres.database.azure.com',
+#           'PORT': '5432',
+#           'OPTIONS': {"sslmode": "require"},
+#      }
+# }
+
 DATABASES = {
      'default': {
-          'ENGINE': 'django.db.backends.postgresql',
-          'NAME': 'django',
-          'USER': 'admin01',
-          'PASSWORD': config('DB_PASSWORD'),
-          'HOST': 'solesforkiddosdb.postgres.database.azure.com',
-          'PORT': '5432',
-          'OPTIONS': {"sslmode": "require"},
+         'ENGINE': 'django.db.backends.postgresql',
+         'URL': config('DATABASE_URL'),
+         'NAME': config('PGDATABASE'),
+         'USER': config('PGUSER'),
+         'PASSWORD': config('PGPASSWORD'),
+         'HOST': config('PGHOST'),
+         'PORT': 6819,
      }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'URL': config('DATABASE_URL'),
-#         'NAME': config('PGDATABASE'),
-#         'USER': config('PGUSER'),
-#         'PASSWORD': config('PGPASSWORD'),
-#         'HOST': config('PGHOST'),
-#         'PORT': 6819,
-#     }
-# }
 
 
 # Password validation
